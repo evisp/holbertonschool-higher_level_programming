15538de685fc01f6f5bc990c17c479d52914443b
evisp <evisplaku@gmail.com>
2022-05-14 10:57:26 -0700

    Add: task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for value in range(x):
         try:
             print(f"{my_list[value]}", end="")
-            lenght += 1
+            length += 1
         except IndexError:  
             break  
     print("")
