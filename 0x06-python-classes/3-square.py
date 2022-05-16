42dbd1c26b22e4f83757f6f1509f18bb814f10d4
evisp <evisplaku@gmail.com>
2022-05-16 12:25:46 -0700

    Add: task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Class Square is defined as an empty class.  """
+
+
+class Square:
+        """ class Square"""
+        def __init__(self, size=0):
+                """private instance size"""
+                if type(size) is not int:
+                        raise TypeError("size must be an integer")
+                if size < 0:
+                        raise ValueError("size must be >= 0")
+                self.__size = size
+
+        def area(self):
+                """ method that computes the area of a square"""
+                return self.__size ** 2
