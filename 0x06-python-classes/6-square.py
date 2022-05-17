1d748d5a3634218cc06053db399469a0105272ea
evisp <evisplaku@gmail.com>
2022-05-17 11:02:19 -0700

    Check

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square:
         """ class Square"""
-        def __init__(self, size=0):
+        def __init__(self, size=0, position=(0, 0)):
                 """private instance size"""
                 if type(size) is not int:
                         raise TypeError("size must be an integer")
@@ -28,14 +28,13 @@ class Square:
         def position(self):
                 """Get/set the current position of the square."""
                 return (self.__position)
-        
+
         @position.setter
         def position(self, value):
                 if type(value) is not tuple or len(value) is not 2 or type(value[0]) is not int or value[0] < 0 or type(value[1]) is not int or value[1] < 0:
                         raise TypeError("position must be a tuple of 2 positive integers")
                 else:
                         self.__position = value
-
 
         def area(self):
                 """ method that computes the area of a square"""
