b97df40bc3dfd50ddc02389e4f0602c1177b9f7f
evisp <evisplaku@gmail.com>
2022-05-20 08:57:51 -0700

    Add: task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,7 +25,6 @@ class Rectangle:
                 raise TypeError("width must be an integer")
             if value < 0:
                 raise ValueError("width must be >= 0")
-
             self.__width = value
 
         @height.setter
