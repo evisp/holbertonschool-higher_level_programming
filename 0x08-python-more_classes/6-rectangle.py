34e17e5d3e2a026b81d1a0b343f7d13770255770
evisp <evisplaku@gmail.com>
2022-05-20 09:26:57 -0700

    Add: task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """ class initialization """
 
     # class attribute number_of_instances
-    number_of_intances = 0
+    number_of_instances = 0
     
     def __init__(self, width=0, height=0):
         """
