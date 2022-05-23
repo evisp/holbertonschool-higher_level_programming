75a3fe2eaf1d44aceca1c7cf634dea9ac374a0d6
evisp <evisplaku@gmail.com>
2022-05-23 03:53:06 -0700

    Add: unit test

---
+++ 0x07-python-test_driven_development/6-max_integer_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_list_ints(self):
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+        self.assertEqual(max_integer([3, 2, 1]), 3)
+        self.assertEqual(max_integer([-1, 0, 1]), 1)
+        self.assertEqual(max_integer([-1, -1, -1]), -1)
+
+    def test_list_floats(self):
+        self.assertEqual(max_integer([1.1, 2.2, 3.3]), 3.3)
+        self.assertEqual(max_integer([-1.1, 0, 1]), 1)
+        self.assertEqual(max_integer([-1, -7.2, 3]), 3)
+
+    def test_string(self):
+        self.assertEqual(max_integer("Jack Sparrow"), 'w')
+        self.assertEqual(max_integer(["Jack", "Sparrow"]),"Sparrow")
+        self.assertEqual(max_integer(""), None)
+
