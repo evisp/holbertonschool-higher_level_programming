28f17315bf5fe8c35dd55bca11f880a295a671eb
evisp <evisplaku@gmail.com>
2022-05-24 11:20:10 -0700

    Add: task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Define class BaseGeometry """
+
+
+class BaseGeometry:
+    """ Define class BaseGeometry """
+    def area(self):
+        raise Exception("area() is not implemented")
