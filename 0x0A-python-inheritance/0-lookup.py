7b5f703ba3ffc65183fedd501c6a9443749dfaa5
evisp <evisplaku@gmail.com>
2022-05-24 10:46:46 -0700

    Add: task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" Method lookup"""
 
 def lookup(obj):
+    """ lookup method
+    Returns:
+        the list of available attributes and methods of an object
+    """
     return dir(obj)
