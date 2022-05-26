9a62f2eec6de07052446f1f5d8dac4a9d74fd93b
evisp <evisplaku@gmail.com>
2022-05-26 10:25:15 -0700

    add: task 10

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" class Student """
+
+
+class Student:
+    """ definition """
+    def __init__(self, first_name, last_name, age):
+        """ instantiation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ public method that retrieves a dictionary representation
+        of a student instance """
+        return self.__dict__
