a36d51577e0dbac4ff3440e99a58597e1ba44e40
evisp <evisplaku@gmail.com>
2022-07-28 11:59:39 -0700

    add: task 12

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -6,7 +6,7 @@ function factorial(number) {
     if (number === 0 || isNaN(number))
 	return 1;
     else
-	return number * factorial(number-1)
+	return number * factorial(number-1);
 }
 
 console.log(factorial(number));
---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const numbers = process.argv;
+
+if (numbers.length <= 3) {
+    console.log('0');
+} else {
+    numbers.sort((a, b) => a - b);
+
+    const numbersArray = numbers.slice(numbers.length - 2);
+    console.log(numbersArray[0]);
+}
