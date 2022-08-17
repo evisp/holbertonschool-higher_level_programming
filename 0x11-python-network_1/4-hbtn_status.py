68b2571b521e07ee83ab9fd912cf9afd640446a4
evisp <evisplaku@gmail.com>
2022-08-17 12:43:13 -0700

    add

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
 
     url = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print(f"\t- type: {type(url.text)}")
-    print(f"\t- content: {url.text}")
+    print("\t- type: {}".format(type(url.text)))
+    print("\t- content: {}".format(url.text))
