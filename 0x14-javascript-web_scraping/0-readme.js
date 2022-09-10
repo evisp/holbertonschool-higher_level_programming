7ae557431125ec44bf8b3b496bfe21cd87bdd6a8
evisp <evisplaku@gmail.com>
2022-09-10 12:18:49 -0700

    add: task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* A script that reads and prints the content of a file. */
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], "utf-8", (error, data) => {
+    if (error) {
+	console.log(error)
+    }
+    console.log(data)
+});
