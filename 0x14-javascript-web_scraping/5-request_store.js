260a2d3ec82efe267ae1ec951c94df4bb7b156e2
evisp <evisplaku@gmail.com>
2022-09-10 13:10:22 -0700

    task 6

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* A script that gets the contents of a webpage and stores it in a file. */
 
-const fs = request('fs')
+const fs = require('fs')
 const request = require('request');
 const url = process.argv[2];
 const file = process.argv[3]
