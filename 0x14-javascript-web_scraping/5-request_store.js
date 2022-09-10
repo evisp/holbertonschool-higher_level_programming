db83e4058bb2a2b0ce554adc6b17f580295f4dde
evisp <evisplaku@gmail.com>
2022-09-10 13:03:48 -0700

    5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/* A script that gets the contents of a webpage and stores it in a file. */
+
+const fs = request('fs')
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3]
+
+request(url, function (error, response, body) {
+    if (error) {
+	console.log(error);
+    }
+    fs.writeFile(file, body, 'utf-8', (error) => {
+	if (error) {
+	    console.log(error)
+	}
+    });
+});
