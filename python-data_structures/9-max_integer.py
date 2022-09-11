cd149b046fbe8a72e7217286b00e013039aead51
evisp <evisplaku@gmail.com>
2022-09-11 00:12:36 -0700

    add task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max = my_list[0]
+    for el in my_list:
+        if el > max:
+            max = el
+    return max  
